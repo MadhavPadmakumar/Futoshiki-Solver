37c62722e01b8f6e519ae8df4ef60c19b06da038
Madhav Padmakumar <padmakma@hotmail.com>
2022-08-26 00:13:10 +1000

    Basic program complete. Conjecture proven for n <= 5.

--- Futoshiki.cpp
+++ Futoshiki.cpp
@@ -4,8 +4,16 @@ using namespace std;
 #include <ranges>
 #include <map>
 
-bool solve(const int n, std::vector<std::vector<int>> &puzzle);
-bool is_valid(const int n, int row, int col, int val, const std::vector<std::vector<int>> &puzzle);
+bool solve(const int n, std::vector<std::vector<int>> &puzzle, std::vector<std::vector<int>> &constraints);
+bool is_valid(const int n, int row, int col, int val, const std::vector<std::vector<int>> &puzzle, std::vector<std::vector<int>> &constraints);
+void generate_constraints(const int n, std::vector<std::vector<int>> &constraints);
+void choose_constraints(const int n,
+						std::vector<std::vector<int>> &puzzle, 
+						std::vector<std::vector<int>> &constraints, 
+						std::vector<std::vector<int>> &allConstraints);
+bool is_valid_constraint(const std::vector<std::vector<int>> &constraints, const std::vector<int> &newConstraint);
+void counterexample(const std::vector<std::vector<int>> &puzzle, const std::vector<std::vector<int>> &constraints);
+						
 
 int main() {
 	int n;
@@ -21,31 +29,14 @@ int main() {
 	generate_constraints(n, allConstraints);
 
 	// Attempt to solve all possible Futoshikis of order n with n-1 constraints.
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			while(constraints.size() < n - 1) {
-				
-			}
-		}
-	}
+	choose_constraints(n, puzzle, constraints, allConstraints);
 
-	// std::vector<std::vector<int>> puzzle = {{1, 2, 3}, {2, 3, 1}, {3, 1, 0}};
-	// std::vector<std::vector<std::vector<int>>> Puzzle = {{{1}, {2}, {3}}, {{2}, {3}, {1}}, {{3}, {1}, {1, 2, 3}}};
-	if (solve(n, puzzle)){
-		cout << "Puzzle solved:" << endl;
-		for (std::vector<int> row : puzzle) {
-			for (int col : row) {
-				cout << col << " ";
-			}
-			cout << endl;
-		}
-	}
-	else {
-		cout << "Puzzle not solvable." << endl;
-	}
+	// If no counterexample was found, conjecture is true for n.
+	cout << "Conjecture is true for " << n << '.' << endl;
 }
 
-bool solve(const int n, std::vector<std::vector<int>> &puzzle) {
+// Recursively solve the puzzle.
+bool solve(const int n, std::vector<std::vector<int>> &puzzle, std::vector<std::vector<int>> &constraints) {
 	int row = 0;
 	int col = 0;
 	int val = 0;
@@ -63,9 +54,9 @@ bool solve(const int n, std::vector<std::vector<int>> &puzzle) {
 		return true;
 	}
 	for (int i = 1; i <= n; i++) {
-		if (is_valid(n, row, col, i, puzzle)) {
+		if (is_valid(n, row, col, i, puzzle, constraints)) {
 			puzzle[row][col] = i;
-			if (solve(n, puzzle)) {
+			if (solve(n, puzzle, constraints)) {
 				return true;
 			}
 			puzzle[row][col] = 0;
@@ -74,21 +65,39 @@ bool solve(const int n, std::vector<std::vector<int>> &puzzle) {
 	return false;
 }
 
-bool is_valid(const int n, int row, int col, int val, const std::vector<std::vector<int>> &puzzle) {
+// Determines if a cell choice is valid.
+bool is_valid(const int n, int row, int col, int val, const std::vector<std::vector<int>> &puzzle, std::vector<std::vector<int>> &constraints) {
+	// No duplicates in row.
 	for (int i = 0; i < n; i++) {
 		if (puzzle[row][i] == val) {
 			return false;
 		}
 	}
+
+	// No duplicates in column.
 	for (int i = 0; i < n; i++) {
 		if (puzzle[i][col] == val) {
 			return false;
+		}
+	}
+
+	// Satisfies inequalities.
+	for (vector<int> constraint : constraints) {
+		if (constraint[0] == row & constraint[1] == col) {
+			if (val > puzzle[constraint[2]][constraint[3]] & puzzle[constraint[2]][constraint[3]] != 0) {
+				return false;
+			}
+		} else if (constraint[2] == row & constraint [3] == col) {
+			if (val < puzzle[constraint[0]][constraint[1]]) {
+				return false;
+			}
 		}
 	}
 	
 	return true;
 }
 
+// Populates a vector with all possible inequality constraints.
 void generate_constraints(const int n, std::vector<std::vector<int>> &constraints) {
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
@@ -106,4 +115,83 @@ void generate_constraints(const int n, std::vector<std::vector<int>> &constraint
 			}
 		}
 	}
+}
+
+// Recursively choose each combination of n-1 constraints, then solve them.
+void choose_constraints(const int n,
+						std::vector<std::vector<int>> &puzzle, 
+						std::vector<std::vector<int>> &constraints, 
+						std::vector<std::vector<int>> &allConstraints) {
+	if (constraints.size() == n - 1) {
+		if (solve(n, puzzle, constraints)) {
+			// Continue recursing.
+			return;
+		} else {
+			// Counterexample found.
+			counterexample(puzzle, constraints);
+			return;
+		}
+	}
+	else {
+		if(constraints.empty()){
+			// Remove each restraint from the list once, then recurse.
+			int steps = allConstraints.size();
+			for(int i = 0; i < steps; i++){
+				constraints.push_back(allConstraints[0]);
+				allConstraints.erase(allConstraints.begin());
+				choose_constraints(n, puzzle, constraints, allConstraints);
+				constraints.pop_back();
+			}
+		} else{
+			// Recurse.
+			for (int i = 0; i < allConstraints.size(); i++) { 
+				if(is_valid_constraint(constraints, allConstraints[i])){
+					constraints.push_back(allConstraints[i]);
+					choose_constraints(n, puzzle, constraints, allConstraints);
+					constraints.pop_back();
+				}
+			}
+		}
+	}
+}
+
+// Checks if candidate constraint is valid.
+bool is_valid_constraint(const std::vector<std::vector<int>> &constraints, const std::vector<int> &newConstraint){
+	// // new constraint cannot be a duplicate of an existing constraint.
+	// if (std::find(constraints.begin(), constraints.end(), newConstraint) != constraints.end()){
+	// 	return false;
+	// }
+
+	for (int i = 0; i < constraints.size(); i++) {
+		// new constraint cannot be a duplicate of an existing constraint.
+		if (constraints[i][0] == newConstraint[0] & constraints[i][1] == newConstraint[1] &
+		    constraints[i][2] == newConstraint[2] & constraints[i][3] == newConstraint[3]) {
+			return false;
+		}
+
+		// new constraint cannot be the opposite of an existing constraint.
+		if (constraints[i][0] == newConstraint[2] & constraints[i][1] == newConstraint[3] &
+		    constraints[i][2] == newConstraint[0] & constraints[i][3] == newConstraint[1]) {
+			return false;
+		}
+	}
+
+	// Constraint is valid.
+	return true;
+}
+
+// Output counterexample and terminate program.
+void counterexample(const std::vector<std::vector<int>> &puzzle, const std::vector<std::vector<int>> &constraints) {
+	cout << "Counterexample found:" << endl;
+	for (int i = 0; i < puzzle.size(); i++) {
+		for (int j = 0; j < puzzle.size(); j++) {
+			cout << puzzle[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	for (int i = 0; i < constraints.size(); i++) {
+		cout << "(" << constraints[i][0] << ", " << constraints[i][1] << ") < (" << constraints[i][2] << ", " << constraints[i][3] << ")" << endl;
+	}
+	exit(0);
 }
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Futoshiki-Solver
 A program for testing a conjecture regarding solvable Futoshiki puzzles.
+
+Conjecture 3.5.6. from Edmond J. Gao's thesis, "Futoshiki: A Latin Square Puzzle Variant":
+    In a blank n by n Futoshiki with less than n inequalities with no loops, there exists at least one solution.
+
+This program attempts to generate all such Futoshiki puzzles for a particular n, then solve them. 
+If it finds a counterexample, it will output it in the terminal.
+
+It has currently proven the conjecture true for n <= 5. It requires more performance optimisation to go further 
+than this in a reasonable amount of time. There are a few methods by which this can occur:
+
+1. Making the solver itself smarter; currently it uses a simple DFS backtracking algorithm, but it could be 
+enhanced with constraint-satisfaction logic. Namely, keeping track of all the possibilities for each cell,
+and gradually ruling them out. Space complexity should be kept in mind when doing this; for example, making a 
+copy of the possibilities for each step of the recursion tree would probably result in a heap crash.
+
+2. Remove reduntant Futoshikis. Some Futoshiki puzzles are equivalent to each other; e.g. flipping or 
+rotating along the centre of the grid, or inverting all the inequalities. If I can find a way of efficiently
+categorising "equivalence classes" of Futoshiki, I can reduce the work by a factor of 32.
+
+3. Using more performant STL/Ranges methods for iteration, as opposed to C-style loops and logic.
